7908dd2703b1bc3e51110e1a1a7da6cfd35a931b
devlopersabbir <devlopersabbir@gmail.com>
2024-04-08 04:44:17 +0600

    [chore 🥸] bun routes

--- index.ts
+++ index.ts
@@ -2,11 +2,14 @@ import { serve } from "bun";
 import figlet from "figlet";
 
 const server = serve({
-  port: 5000,
+  port: Bun.env["PORT"],
   hostname: "localhost",
   fetch(req) {
-    console.log(figlet.textSync(req.method));
-    return new Response("Hello world");
+    const url = new URL(req.url);
+    figlet.textSync(req.url);
+    if (url.pathname === "/") return new Response("<h1>Home page</h1>");
+    if (url.pathname === "/blog") return new Response("<h1>Blog page</h1>");
+    return new Response("<h1>404 Not found!</h1>");
   },
 });
 
